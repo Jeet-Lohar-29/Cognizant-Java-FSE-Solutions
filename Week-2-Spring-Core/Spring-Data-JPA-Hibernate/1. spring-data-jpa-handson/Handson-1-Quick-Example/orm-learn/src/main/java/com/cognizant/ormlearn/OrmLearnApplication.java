aef0acc5c770d7959153365f1b9936d26e0105d2
Jeet Lohar <jeetloharcrj@gmail.com>
2026-07-13 09:26:33 +0530

    Completed Handson 1 - Spring Data JPA Quick Example

---
+++ Week-2-Spring-Core/Spring-Data-JPA-Hibernate/1. spring-data-jpa-handson/Handson-1-Quick-Example/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,29 @@
+package com.cognizant.ormlearn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.cognizant.ormlearn.service.CountryService;
+
+@SpringBootApplication
+public class OrmLearnApplication implements CommandLineRunner {
+
+    @Autowired
+    private CountryService countryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmLearnApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        System.out.println("Countries in Database:");
+
+        countryService.getAllCountries()
+                .forEach(System.out::println);
+
+    }
+}
