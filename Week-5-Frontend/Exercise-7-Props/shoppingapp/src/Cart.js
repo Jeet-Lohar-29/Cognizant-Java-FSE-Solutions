09123ec523061d585c78866019e9bf744a42aa6f
Jeet Lohar <jeetloharcrj@gmail.com>
2026-07-23 09:33:12 +0530

    Completed Week 5 Exercise 7 - React Props

---
+++ Week-5-Frontend/Exercise-7-Props/shoppingapp/src/Cart.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+class Cart extends Component {
+
+    render() {
+
+        return (
+
+            <table
+                border="1"
+                cellPadding="5"
+                style={{
+                    margin: "auto",
+                    borderCollapse: "collapse"
+                }}
+            >
+
+                <thead>
+
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+
+                </thead>
+
+                <tbody>
+
+                    {
+                        this.props.item.map((item, index) => (
+
+                            <tr key={index}>
+
+                                <td>{item.itemname}</td>
+
+                                <td>{item.price}</td>
+
+                            </tr>
+
+                        ))
+                    }
+
+                </tbody>
+
+            </table>
+
+        );
+
+    }
+
+}
+
+export default Cart;
